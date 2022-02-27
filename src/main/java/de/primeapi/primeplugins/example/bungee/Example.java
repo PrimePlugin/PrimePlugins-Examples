4beaf15a88bb237e256bf6548bf63c15b43e9dc3
Exceptionpilot <drach.jonas@web.de>
2022-02-27 11:41:43 +0100

    (test)

---
+++ src/main/java/de/primeapi/primeplugins/example/bungee/Example.java
@@ -0,0 +1,10 @@
+package de.primeapi.primeplugins.example.bungee;
+
+/**
+ * @author Jonas D. Exceptionpilot
+ * created on 27.02.2022
+ * created for PrimePlugins-Examples
+ */
+
+public class Example {
+}
